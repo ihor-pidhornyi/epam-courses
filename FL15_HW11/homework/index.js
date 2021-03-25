198311c293b412b0177c65bd9c80d1f09eb6c499
MapIIeX <podhorhyi_2002@ukr.net>
2021-03-25 13:43:13 +0200

    hw11-improvement

--- FL15_HW11/homework/index.js
+++ FL15_HW11/homework/index.js
@@ -3,19 +3,17 @@ const isBigger = (a, b) => a > b;
 const storeNames = (...names) => names;
 const getDifference = (a, b) => a > b ? a - b : b - a;
 const negativeCount = arr => arr.reduce((acc, cur) => acc + (cur < 0 ? 1 : 0), 0);
-const letterCount = (str, letter) => str.length - str.split(letter).join('').length;
+const letterCount = (str, letter) => str.length - str.toLowerCase().split(letter.toLowerCase()).join('').length;
 const countPoints = arr => {
-    const POINTS_FOR_WIN = 3,
-        POINTS_FOR_DRAW = 1,
-        POINTS_FOR_DEFEAT = 0;
+    const POINTS_FOR_WIN = 3;
     return arr.reduce((acc, cur) => {
         const [our, enemy] = cur.split(':').map(el => Number(el));
         if (our > enemy) {
             return acc + POINTS_FOR_WIN;
         } else if (our === enemy) {
-            return acc + POINTS_FOR_DRAW;
+            return acc + 1;
         } else {
-            return acc + POINTS_FOR_DEFEAT;
+            return acc + 0;
         }
     }, 0);
 }
