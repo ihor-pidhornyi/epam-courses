4176ba28e9e7ca567e882b1d09d7b42f5fe334e4
MapIIeX <podhorhyi_2002@ukr.net>
2021-03-26 21:44:22 +0200

    hw12-pipe-change

--- FL15_HW12/homework/js/pipe.js
+++ FL15_HW12/homework/js/pipe.js
@@ -3,26 +3,29 @@ function isFunction(functionToCheck) {
 }
 
 const pipe = (value, ...funcs) => {
-	for(let i = 0; i < funcs.length; i++) {
-		if(!isFunction(funcs[i])) {
-			throw new Error(`Provided argument at position ${i} is not a function!`);
+	try {
+		return funcs.reduce((result, currFunction) => currFunction(result), value);
+	} catch(e) {
+		for(let i = 0; i < funcs.length; i++) {
+			if(!isFunction(funcs[i])) {
+				return `Provided argument at position ${i} is not a function!`;
+			}
 		}
-	}
-	return funcs.reduce((result, currFunction) => currFunction(result), value);
+	}		
 };
 
-const replaceUnderscoreWithSpace = (value) => value.replace(/_/g, ' ');
-const capitalize = (value) =>
-	value
-		.split(' ')
-		.map((val) => val.charAt(0).toUpperCase() + val.slice(1))
-		.join(' ');
-const appendGreeting = (value) => `Hello, ${value}!`;
+// const replaceUnderscoreWithSpace = (value) => value.replace(/_/g, ' ');
+// const capitalize = (value) =>
+// 	value
+// 		.split(' ')
+// 		.map((val) => val.charAt(0).toUpperCase() + val.slice(1))
+// 		.join(' ');
+// const appendGreeting = (value) => `Hello, ${value}!`;
 
-const error = pipe('john_doe', replaceUnderscoreWithSpace, capitalize, '');
+// const error = pipe('john_doe', replaceUnderscoreWithSpace, capitalize, '');
 
-alert(error); // Provided argument at position 2 is not a function!
+// alert(error); // Provided argument at position 2 is not a function!
 
-const result = pipe('john_doe', replaceUnderscoreWithSpace, capitalize, appendGreeting);
+// const result = pipe('john_doe', replaceUnderscoreWithSpace, capitalize, appendGreeting);
 
-alert(result); // Hello, John Doe!
+// alert(result); // Hello, John Doe!
