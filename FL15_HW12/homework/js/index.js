d71a3e96ca773607773dc28c063f8b044e44dc3a
MapIIeX <podhorhyi_2002@ukr.net>
2021-03-26 21:08:15 +0200

    hw12-task1-fix

--- FL15_HW12/homework/js/index.js
+++ FL15_HW12/homework/js/index.js
@@ -1,8 +1,5 @@
 function visitLink(path) {
 	const obj = JSON.parse(localStorage.getItem('pagesClicked')) || {};
-	if(!Object.keys(obj).length) {
-		setDefaultData(obj);
-	}
 	obj[path] = obj[path] ? ++obj[path] : 1;
 	localStorage.setItem('pagesClicked', JSON.stringify(obj));
 }
@@ -10,34 +7,19 @@ function visitLink(path) {
 function viewResults() {
 	const wrapper = document.getElementById('content');
 	const pages = JSON.parse(localStorage.getItem('pagesClicked')) || {};
-	if (!Object.keys(pages).length) {
-		setDefaultData(pages);
-	}
 
-	if (wrapper && !document.querySelector('#list')) {
-		const list = document.createElement('ul');
+	let list = document.querySelector('#list');
+	if (!list) {
+		list = document.createElement('ul');
 		list.id = 'list';
-		for (let page in pages) {
-			const row = document.createElement('li');
-			row.textContent = `You visited ${page} ${pages[page]} time(s)`;
-			list.append(row);
-		}
-		wrapper.append(list);
 	} else {
-		const liList = document.querySelector("#list").childNodes;
-		let i = 0;
-		for(let page in pages) {
-			liList[i].textContent = `You visited ${page} ${pages[page]} time(s)`;
-			i++;
-		}
+		list.innerHTML = '';
 	}
+	for(let page in pages) {
+		const row = document.createElement('li');
+		row.textContent = `You visited ${page} ${pages[page]} time(s)`;
+		list.append(row);
+	}
+	wrapper.append(list);
 	localStorage.clear();
 }
-
-function setDefaultData(obj) {
-	const pageNodes = document.querySelectorAll('.nav-item');
-	for (let i = 1; i < pageNodes.length; i++) {
-		obj[`Page${i}`] = 0;
-	}
-	localStorage.setItem('pagesClicked', JSON.stringify(obj));
-};
